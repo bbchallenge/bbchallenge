a244ea64a509b1b95f17822d0cff494e66d04786
Tristan Stérin <tristan.sterin@mu.ie>
2022-03-27 16:46:51 +0100

    no me

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1035,9 +1035,3 @@
 		</div>
 	</div>
 </div>
-
-<div class="text-[0.6rem]">
-	Maintainer: <a href="https://dna.hamilton.ie/tsterin/" class="text-blue-400 hover:text-blue-300"
-		>Tristan Stérin</a
-	>
-</div>
