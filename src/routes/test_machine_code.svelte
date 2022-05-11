aa89a3d2bbbb1da1dd75feebe8d2e41916f4fa24
Tristan Stérin <tristan.sterin@mu.ie>
2022-05-11 20:47:23 +0100

    ground work changing tm format

---
+++ src/routes/test_machine_code.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  //Making sure that encoding of machines is same on server and client
+	import { API } from '$lib/api_server';
+	import {machineCodeToTM, tmToMachineCode } from '$lib/tm';
+  import { onMount } from 'svelte';
+
+  async function getRandomMachine() {
+		try {
+			const response = await API.post('/machine/random', { type: "all" });
+
+			return response.data['machine_code']
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+  onMount(async () => {
+  let nb_tests = 1000;
+
+    for (let i = 0 ; i < nb_tests ; i += 1) {
+      let machine_code = await getRandomMachine()
+        let machine = machineCodeToTM(machine_code);
+        let machine_code_again = tmToMachineCode(machine);
+        if (machine_code_again != machine_code) {
+          console.log("Problem", machine, machine_code, machine_code_again)
+        }
+    }
+  })
+
+</script>
