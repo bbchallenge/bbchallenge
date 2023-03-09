b9bb22ab52286eb0d4e9db55ad19afceb8eed13c
Alexandre Jouandin <alexandre@jouand.in>
2023-03-09 15:36:35 +0000

    Upgrade `netlify-cli` and remove `cors-backdoor` and `svelte-seo`

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -24,6 +24,10 @@
 	}
 </script>
 
+<svelte:head>
+	<title>Busy Beaver Challenge</title>
+</svelte:head>
+
 <div class="p-3">
 	<nav
 		class="lg:px-2 px-0 pb-2 flex justify-between lg:h-9 border-b-2 flex-col lg:flex-row h-14 mt-1"
