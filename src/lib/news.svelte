4dfc9362e3543fb0e6d90f2eba2ee2833efb78f2
Tristan Stérin <tristan@prgm.dev>
2025-09-30 22:25:59 +0200

    new news

--- src/lib/news.svelte
+++ src/lib/news.svelte
@@ -11,6 +11,9 @@
 		</div>
 		<ul class="flex flex-col space-y-2 mt-2 list-disc">
 			<li>
+				<a href="https://arxiv.org/abs/2509.12337">September 15th 2025</a>
+			</li>
+			<li>
 				<a
 					href="https://discuss.bbchallenge.org/t/july-2nd-2024-we-have-proved-bb-5-47-176-870/237"
 				>
@@ -23,12 +26,12 @@
 					><span class="underline">May 2nd 2024</span></a
 				>
 			</li>
-			<li>
+			<!-- <li>
 				<a
 					href="https://discuss.bbchallenge.org/t/april-9th-2023-503-169-new-machines-have-been-decided-only-32-632-left-to-go/"
 					><span class="underline">April 9th 2023</span></a
 				>
-			</li>
+			</li> -->
 			<!-- <li>
 				<a
 					href="https://discuss.bbchallenge.org/t/december-25th-2022-1-002-823-new-machines-have-been-decided/"
